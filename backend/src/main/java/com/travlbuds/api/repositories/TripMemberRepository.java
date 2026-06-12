bc6a5f6c05cf4e33a640ab3ed58a4b0e559ed69e
owenqren <131617292+owenqren@users.noreply.github.com>
2026-06-12 18:13:29 -0400

    Remove autowired and redundant Repository

--- backend/src/main/java/com/travlbuds/api/repositories/TripMemberRepository.java
+++ backend/src/main/java/com/travlbuds/api/repositories/TripMemberRepository.java
@@ -2,11 +2,9 @@ package com.travlbuds.api.repositories;
 
 import com.travlbuds.api.models.TripMember;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 import java.util.List;
 import java.util.Optional;
 
-@Repository
 public interface TripMemberRepository extends JpaRepository<TripMember, Long> {
     List<TripMember> findByTripId(Long tripId);
 
